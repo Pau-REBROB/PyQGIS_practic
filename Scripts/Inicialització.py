c3edcba44c58cd4dea36d23d475ad22ab44a5a63
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-03-28 14:48:46 +0100

    Refactor layer paths and improve comments in Inicialització.py
    Updated file paths for administrative limits and road graph layers. Enhanced comments for clarity.

--- Scripts/Inicialització.py
+++ Scripts/Inicialització.py
@@ -12,54 +12,60 @@ root = project.layerTreeRoot()
 
 # Diccionari de capes per temàtica amb el nom i la ruta
 capes_lim_adm = {
-    "Barris": "d:/47994558K/OneDrive - Generalitat de Catalunya/QGIS_TESTS/BARCELONA/Limits_Administratius/BCN_UNITATS_ADM_SHP/0301040100_Barris_UNITATS_ADM.shp",
-    "Districtes": "d:/47994558K/OneDrive - Generalitat de Catalunya/QGIS_TESTS/BARCELONA/Limits_Administratius/BCN_UNITATS_ADM_SHP/0301040100_Districtes_UNITATS_ADM.shp",
-    "TermeMunicipal": "d:/47994558K/OneDrive - Generalitat de Catalunya/QGIS_TESTS/BARCELONA/Limits_Administratius/BCN_UNITATS_ADM_SHP/0301040100_TermeMunicipal_UNITATS_ADM.shp"
+    "Barris": "C:/PyQGIS_practic/Limits_administratius_BCN/0301040100_Barris_UNITATS_ADM.shp",
+    "Districtes": "C:/PyQGIS_practic/Limits_administratius_BCN/0301040100_Districtes_UNITATS_ADM.shp",
+    "TermeMunicipal": "C:/PyQGIS_practic/Limits_administratius_BCN/0301040100_TermeMunicipal_UNITATS_ADM.shp"
 }
 
 capes_graf_viari = {
-    "Graf_trams": "d:/47994558K/OneDrive - Generalitat de Catalunya/QGIS_TESTS/BARCELONA/Graf_viari/BCN_GrafVial_Trams_ETRS89_SHP.shp",
-    #"Graf_nodes": "d:/47994558K/OneDrive - Generalitat de Catalunya/QGIS_TESTS/BARCELONA/Graf_viari/BCN_GrafVial_Nodes_ETRS89_SHP.shp"
+    "Graf_trams": "C:/PyQGIS_practic/Graf_viari/BCN_GrafVial_Trams_ETRS89_SHP.shp",
+    #"Graf_nodes": "C:/PyQGIS_practic/Graf_viari/BCN_GrafVial_Nodes_ETRS89_SHP.shp"
 }
 
+# Diccionari buit dels diccionaris de capes per temàtica
 layers = {
     "Limits_administratius": {},
     "Graf": {}
 }
+
+# Diccionari buit dels índex de les capes
 indexs = {}
 
-# Importació de les capes
-# Generació dels índex espacials sobre les geometries de cada capa
+# Importació de les capes al projecte i generació dels índex espacials sobre les geometries de cada capa
 ## Límits administratius
 for i, (nom, path) in enumerate(capes_lim_adm.items()):
-    # Creació de la capa vectorial
+    # Per a cada nom i ruta del diccionari de capes de límits administratius:
+    # Creació de la capa vectorial amb la ruta i utilitzant el nom
     layer = QgsVectorLayer(path, nom, "ogr")
     # Comprovació que la capa és vàlida
     if not layer.isValid():
         print(f"Error al carregar la capa de {nom}!")
     else:
-        # Addició de la capa al projecte
+        # Si la capa és vàlida:
+        # Addició de la capa al projecte, sense afegir-la al llenç
         project.addMapLayer(layer, False)
         # Inserció de la capa al panell de capes a la posició definida
         root.insertLayer(i, layer)
-        # Addició de la capa al diccionari de capes
+        # Addició de la capa al diccionari de diccionaris de capes, al grup de Límits administratius
         # El key és el nom de la capa, i el value és la capa vectorial pròpiament (QgsVectorLayer)
         layers["Limits_administratius"][nom] = layer
         # Generació de l'índex i addició al diccionari d'índexs
         indexs[nom] = QgsSpatialIndex(layer.getFeatures())
 ## Graf viari
 for i, (nom, path) in enumerate(capes_graf_viari.items()):
-    # Creació de la capa vectorial
+    # Per a cada nom i ruta del diccionari de capes del graf viari:
+    # Creació de la capa vectorial amb la ruta i utilitzant el nom
     layer = QgsVectorLayer(path, nom, "ogr")
     # Comprovació que la capa és vàlida
     if not layer.isValid():
         print(f"Error al carregar la capa de {nom}!")
     else:
-        # Addició de la capa al projecte
+        # Si la capa és vàlida:
+        # Addició de la capa al projecte, sense afegir-la al llenç
         project.addMapLayer(layer, False)
         # Inserció de la capa al panell de capes a la posició definida
         root.insertLayer(i, layer)
-        # Addició de la capa al diccionari de capes
+        # Addició de la capa al diccionari de diccionaris de capes, al grup de Graf viari
         # El key és el nom de la capa, i el value és la capa vectorial pròpiament (QgsVectorLayer)
         layers["Graf"][nom] = layer
         # Generació de l'índex i addició al diccionari d'índexs
