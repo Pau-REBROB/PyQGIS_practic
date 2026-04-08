01db734b5a367f1f687b21b5a6090fc0aed43345
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-04-08 20:02:46 +0200

    Enhance documentation for vector layer import in QGIS
    Added comments explaining how to import and add a vector layer in QGIS.

--- Scripts/01_Capes.py
+++ Scripts/01_Capes.py
@@ -1,8 +1,17 @@
-"""Execució implícita quan s'inicialitza la consola Python de QGIS"""
-from qgis.core import *
-import qgis.utils
-
+"""Capes vectorials"""
 
 # En el supòsit que no es treballi a la consola Python de QGIS
 import os
 from qgis.core import QgsVectorLayer
+
+# Per importar i afegir al projecte una capa vectorial cal crear una instància de la classe `QgsVectorLayer`
+# Cal especificar:
+## Ruta on es troba la capa (*source*)
+## Nom que es desitja donar (*layer name*) com a identificador en el panell de capes
+## Proveïdor de dades vectorials
+vlayer = QgsVectorLayer("Filepath_capa", "Nom_capa", "Proveïdor")
+
+# El proveïdor "ogr" de la llibreria GDAL suporta gran varietat de formats, incloent els Shapefile, GeoJSON, GeoPackage i DXF d'AutoCAD
+# En el cas dels geopackage, cal especificar la capa que es vol importar
+vlayer = QgsVectorLayer("Projecte/Dades/dades.gpkg|layername=Aeroports", "Aeroports", "ogr")
+
