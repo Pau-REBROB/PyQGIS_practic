8ac91df1998e172ba504c3b29a8bacba2e4c9b29
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-04-09 15:37:17 +0200

    Update 01_Capes.py

--- Scripts/01_Capes.py
+++ Scripts/01_Capes.py
@@ -32,7 +32,7 @@ from qgis.core import QgsRasterLayer
 vlayer = QgsRasterLayer("Filepath_capa", "Nom_capa", "Proveïdor")
 
 # De nou, GDAL és el proveïdor que suporta la majoria de formats
-rlayer = QgsRasterLayer("Projecte/Dades/dades.gpkg|layername=dem", "DEM", "gdal")
+rlayer = QgsRasterLayer("GPKG:Projecte/Dades/dades.gpkg:dem", "DEM", "gdal")
 
 # Altres proveïdors disponibles permeten establir connexions WMS ("wms") o carregar capes des d'un servidor PostgreSQL ("postgresraster")
 
@@ -41,6 +41,7 @@ rlayer = QgsRasterLayer("Projecte/Dades/dades.gpkg|layername=dem", "DEM", "gdal"
 
 # Per a afegir una capa ja importada al llenç cal afegir-la a la instància del projecte utilitzant el mètode `.addMapLayer()`
 project.addMapLayer(vlayer)
+project.addMapLayer(rlayer)
 
 # Aquest mètode, declarant la capa amb una instància QgsVectorLayer/QgsRasterLayer + utilitzant la instància del projecte, és el mètode més indicat per a importar capes
 # De manera més ràpida i alternativa, es pot fer ús del mètode `addVectorLayer()` o `addRasterLayer()` de la classe `QgsInterface` per a carregar i visualitzar una capa
@@ -51,4 +52,9 @@ rlayer = iface.addRasterLayer()
 ## Nom que es desitja donar (*layer name*) com a identificador en el panell de capes
 ## Proveïdor de dades vectorials
 # `iface` - o QgsInterface - és una instància proporcionada per QGIS que enllaça la GUI amb el codi Python que, internament, utilitza el mètode indicat anterior (és un *wrapper*) 
-# Per tant, ÚNICAMENT es pot fer servir quan es treballa a la GUI, és a dir, a la consola Python de QGIS 
+# Per tant, ÚNICAMENT es pot fer servir quan es treballa a la GUI, és a dir, a la consola Python de QGIS
+
+# Es pot parlar d'un tercer mètode per afegir capes a un projecte
+# Si al mètode `.addMapLayer()` s'afegeix el paràmetre *False* en segona posició - corresponent a l'argument *addToLegend* -, la capa s'afegeix al projecte, però no al llenç del mapa
+project.addMapLayer(vlayer, False)
+# La capa segueix activa dins del projecte, tal i com reflectirà l'arbre de capes (*layer tree*)
