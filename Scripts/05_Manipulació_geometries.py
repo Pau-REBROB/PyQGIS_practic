1b76513b3bae556d4f8d7b2a70182d6c75bf156b
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-04-17 10:37:08 +0200

    Add initial script for geometric manipulations

---
+++ Scripts/05_Manipulació_geometries.py
@@ -0,0 +1,11 @@
+"""Geometries vectorials"""
+
+#ff
+import
+
+# Les geometries disponibles a QGIS estan representades per la classe `QgsGeometry`
+# Totes segueixen els estàndards de l'OGC de Standard Feature Access
+
+# Es poden generar geometries de 3 maneres diferents
+## A partir de coordenades
+### 
