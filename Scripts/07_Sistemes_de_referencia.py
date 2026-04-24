ab6d801dd92f85693517e56a9cf793fc8f0a1099
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-04-24 11:08:06 +0200

    Add initial script for coordinate reference systems

---
+++ Scripts/07_Sistemes_de_referencia.py
@@ -0,0 +1,9 @@
+# En el supòsit que no es treballi a la consola Python de QGIS
+from qgis.core import (
+    QgsCoordinateReferenceSystem,
+    QgsCoordinateTransform,
+)
+
+
+"""Sistemes de referència de coordenades"""
+
