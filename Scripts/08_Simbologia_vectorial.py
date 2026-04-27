793fb9324506f5dbd3f9ab00c96aaaa36151e8a8
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-04-27 11:59:18 +0200

    Update 08_Simbologia_vectorial.py

--- Scripts/08_Simbologia_vectorial.py
+++ Scripts/08_Simbologia_vectorial.py
@@ -4,3 +4,10 @@
 """Simbologia de capes vectorials"""
 
 # Existeixen
+# Cuando una capa vectorial se representa, la apariencia de los datos se indica por renderer y símbolos asociados a la capa. Los símbolos son clases que se encargan del dibujo de la representación visual de las entidades,
+# mientras que los renderizadores determinan qué símbolo se utilizará para una entidad determinada.
+
+renderer = layer.renderer()
+
+# Per comprovar el vigent
+renderer = layer.renderer().type()
