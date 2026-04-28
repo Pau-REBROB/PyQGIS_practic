f924f05edb61bbfbce53b606c9b08191767fec29
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-04-28 09:02:24 +0200

    Update comments and symbol handling in vector layer script

--- Scripts/08_Simbologia_vectorial.py
+++ Scripts/08_Simbologia_vectorial.py
@@ -3,11 +3,30 @@
 
 """Simbologia de capes vectorials"""
 
-# Existeixen
-# Cuando una capa vectorial se representa, la apariencia de los datos se indica por renderer y símbolos asociados a la capa. Los símbolos son clases que se encargan del dibujo de la representación visual de las entidades,
-# mientras que los renderizadores determinan qué símbolo se utilizará para una entidad determinada.
+# La representació d'una capa vectorial necessita de la definició de símbols associats a la capa, així com d'un renderitzador (*renderer*) que determini el tipus de simbolització que es vol donar
 
-renderer = layer.renderer()
+# Els tipus de simbologia - els tipus de renderers - disponibles son les següents
+print(QgsApplication.rendererRegistry().renderersList())
+# ['nullSymbol', 'singleSymbol', 'categorizedSymbol', 'graduatedSymbol', 'RuleRenderer',
+# 'pointDisplacement', 'pointCluster', 'mergedFeatureRenderer', 'invertedPolygonRenderer', 'heatmapRenderer', '25dRenderer', 'embeddedSymbol']
 
-# Per comprovar el vigent
-renderer = layer.renderer().type()
+# Per comprovar el renderer vigent d'entre els anteriors possibles en una capa vectorial s'utilitza el mètode `.type()`
+vlayer.renderer().type()
+
+# És bon costum guardar el renderitzador en una variable independent
+renderer = vlayer.renderer()
+
+
+# Els símbols tenen diferents classes, segons el tipus de simbologia
+# La classe utilitzada, que acostuma a guardar-se en la variable `symbol`, s'ha d'entendre com un contenidor de capes de simbologia que es poden anar afegint per a fer la simbologia més rica
+## Amb el mètode `.defaultSymbol` es crea un objecte de símbol en funció del tipus de geometria de la capa vectorial
+symbol = QgsSymbol.defaultSymbol(vlayer.geometryType())
+## Utilitzant el renderer es pot extreure la simbologia existent d'una capa
+symbol = vlayer.renderer().symbol()
+
+
+
+
+
+
+
