ee4c182d23ec9b17c6c3a2ea8a6a9cd819e38484
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-05-13 19:51:15 +0200

    Implement new_algorithm class for QGIS processing
    Added a new processing algorithm class with parameters and methods.

--- Scripts/09_Caixa_eines.py
+++ Scripts/09_Caixa_eines.py
@@ -24,4 +24,56 @@ project.addMapLayer(result['OUTPUT'])
 
 """Noves eines de processament"""
 
-#
+# Un mateix pot crear noves eines de processament de la classe `processing` i afegir-les a la caixa d'eines de QGIS
+# El procediment comença declarant una nova classe d'un algoritme dins de processing
+class new_algorithm(QgsProcessingAlgorithm):
+  # Declaració dels paràmetres i constants
+  # PyQGIS treballa amb claus de text internes
+  entry_param_name = "entry_param_name"
+  exit_param_name = "exit_param_name"
+
+  # Declaració de l'identificador de l'algoritme utilitzat per cridar la funció (en minúscules)
+  def name(self):
+    return "new_algorithm"
+
+  # Declaració del nom llegible per humans de l'algoritme, que es veu a la GUI
+  def displayName(self):
+    return "new_algorithm_display_name"
+
+  # Declaració de la nova instància de la classe de l'algoritme
+  def createInstance(self):
+    return type(self)()
+
+  # Configuració de les definicions dels paràmetres d'entrada i de sortida
+  ## Aquells definits al principi de tot
+  # Es defineix la interfície de l'algoritme, que construeix automàticament el formulari UI, la validació i l'ajuda
+  def initAlgorithm(self, config=None):
+    self.addParameter(
+      # Si es demana una capa vectorial o ràster d'entrada
+      QgsProcessingParameterFeatureSource(self.entry_param_name, "Description"),
+      # Si es demana un nombre d'entrada
+      QgsProcessingParameterNumber(self.entry_param_name, "Description"),
+      ## Si el nombre demanat tingués un valor per defecte
+      QgsProcessingParameterNumber(self.entry_param_name, "Description", QgsProcessingParameterNumber.Double, 2),
+      # Si es defineix una capa vectorial o ràster de sortida
+      ## És un element abstracte, que posteriorment s'utilitzarà per a generar la capa de sortida
+      QgsProcessingParameterFeatureSink(self.exit_param_name, "Description")
+    )
+
+  # Definició de les operacions pròpiament dites
+  def processAlgorithm(self, parameters, context, feedback):
+    #
+    #
+
+    # El *context* recull el context del projecte: capes carregades, SRC, gestió de la memòria, etc.
+    # És un objecte de la classe `QgsProcessingContext`
+
+    # El *feedback* permet comunicar-se amb l'usuari
+    # Permet mostrar el progrés (`feedback.setProgress()`), la cancel·lació o els missatges (`feedback.pushInfo()`, `feedback.pushWarning()`, `feedback.reportError()`) 
+    # És un objecte de la classe `QgsProcessingFeedback`
+
+    # Els *helpers* 
+
+
+
+
