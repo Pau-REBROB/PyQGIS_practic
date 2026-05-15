816de8aa0f8ff0b53037564945f6abf6c9882af9
Pau-REBROB <pau.reb.rob@gmail.com>
2026-05-15 07:31:35 +0200

    Actualització arxiu 10

--- Scripts/10_Cartografia.py
+++ Scripts/10_Cartografia.py
@@ -1,3 +1,7 @@
 """Generació i exportació de cartografia"""
 
 # La classe central per al disseny de mapes - el que s'anomena *layouts* - és la classe `QgsLayout`
+
+# Es comença creant una instància d'aquesta classe que, seguidament, cal inicialitzar
+layout = QgsPrintLayout(project)
+layout.initializeDefaults()
