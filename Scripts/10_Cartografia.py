a3ab2b80a3c4c84812b4ffd9d5cecaee33e7b63e
Pau-REBROB <pau.reb.rob@gmail.com>
2026-05-15 16:02:15 +0200

    Actualització arxiu 10

--- Scripts/10_Cartografia.py
+++ Scripts/10_Cartografia.py
@@ -26,15 +26,15 @@ map = QgsLayoutItemMap(layout)
 map.attemptResize(QgsLayoutSize(x,y,units))
 # Per a posicionar-lo, s'utilitza el mètode `.attemptMove()`
 map.attemptMove(QgsLayoutPoint(x,y,units))
-## Les unitats de mida i posició son objectes de la classe `QgsUnitTypes.LayoutMillimeters` o `QgsUnitTypes.LayoutPixels`
+## Les unitats (*units*) de mida i posició son variables tipus `QgsUnitTypes.LayoutMillimeters` o `QgsUnitTypes.LayoutPixels`
 
-# Per a establir una extensió, s'utilitza el mètode `.setExtent()`
-extent = map.setExtent()
+# Per a establir una extensió, s'utilitza algun dels diferents mètodes següents 
 ## Es pot seleccionar l'extensió del mapa amb `map.extent()`
 ## Es pot seleccionar l'extensió actual del canvas amb `iface.mapCanvas().extent()`
 ## Es pot seleccionar l'extensió màxima de totes les capes del panell de capes del projecte amb `project.layerTreeRoot().extent()`
 ## Es pot definir manualment amb `QgsRectangle(xmin, ymin, xmax, ymax)`
-# Establerta l'extensió, cal forçar la seva renderització
+# Establerta l'extensió, s'assigna al mapa amb el mètode `.setExtent()` i, posteriorment, cal forçar la seva renderització 
+map.setExtent(extent)
 map.zoomToExtent(extent)
 
 # Finalment, s'afegeix el mapa a la composició
@@ -83,3 +83,19 @@ layout.addLayoutItem(scale)
 # Per a qualsevol element, la seva posició per defecte és la (0,0), corresponent a la cantonada superior esquerra del canvas
 # Per tant, valors de X superiors centren verticalment l'element
 # Mentre que, valors de Y superiors centren l'element horitzontalment
+
+
+# Definit el layout, cal afegir-lo al gestor de layouts del projecte perquè no quedi en memòria
+# Es defineix un objecte gestor de composicions
+manager = project.layoutManager()
+# S'afegeix el layout amb el mètode `.addLayout()`
+manager.addLayout(layout)
+
+
+# L'exportació de la composició es duu a terme amb un objecte de la classe `QgsLayoutExporter`
+exporter = QgsLayoutExporter(layout)
+# El mètode `.exportToX()` permet exportar la composició a arxiu PDF o a imatge format PNG
+## Exportar a PDF
+exporter.exportToPdf("ruta/output.pdf", QgsLayoutExporter.PdfExportSettings())
+## Exportar a imatge
+exporter.exportToImage("ruta/output.png", QgsLayoutExporter.ImageExportSettings())
