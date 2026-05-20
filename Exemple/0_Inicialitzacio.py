510bba838e67be7d02f6a4dfaa41e829a7594693
Pau-REBROB <pau.reb.rob@gmail.com>
2026-05-20 17:16:05 +0200

    Actualització exemple pràctic. Addició capes urbanisme

--- Exemple/0_Inicialitzacio.py
+++ Exemple/0_Inicialitzacio.py
@@ -7,66 +7,105 @@ project = QgsProject.instance()
 root = project.layerTreeRoot()
 
 
-
 """IMPORTACIÓ DE CAPES"""
 
-# Diccionari de capes per temàtica amb el nom i la ruta
+# Diccionari de capes per temàtica 
+# Nom de la capa: ruta absoluta
 capes_lim_adm = {
-    "Barris": "C:/PyQGIS_practic/Limits_administratius_BCN/0301040100_Barris_UNITATS_ADM.shp",
-    "Districtes": "C:/PyQGIS_practic/Limits_administratius_BCN/0301040100_Districtes_UNITATS_ADM.shp",
-    "TermeMunicipal": "C:/PyQGIS_practic/Limits_administratius_BCN/0301040100_TermeMunicipal_UNITATS_ADM.shp"
+    "Barris": "C:/projectes_git/Dades/PyQGIS_Repo/Limits_administratius_BCN/0301040100_Barris_UNITATS_ADM.shp",
+    "Districtes": "C:/projectes_git/Dades/PyQGIS_Repo/Limits_administratius_BCN/0301040100_Districtes_UNITATS_ADM.shp",
+    "TermeMunicipal": "C:/projectes_git/Dades/PyQGIS_Repo/Limits_administratius_BCN/0301040100_TermeMunicipal_UNITATS_ADM.shp"
+}
+
+capes_urb = {
+    "Adreces": "C:/projectes_git/Dades/PyQGIS_Repo/Urbanisme/0501040100_Adreces_TXT.shp",
+    "Parcelles": "C:/projectes_git/Dades/PyQGIS_Repo/Urbanisme/0601040100_Parcel·lari_POL_v.shp",
+    "Illes": "C:/projectes_git/Dades/PyQGIS_Repo/Urbanisme/0601040100_Illes_POL_v.shp"
 }
 
 capes_graf_viari = {
-    "Graf_trams": "C:/PyQGIS_practic/Graf_viari/BCN_GrafVial_Trams_ETRS89_SHP.shp",
-    #"Graf_nodes": "C:/PyQGIS_practic/Graf_viari/BCN_GrafVial_Nodes_ETRS89_SHP.shp"
+    "Graf_trams": "C:/projectes_git/Dades/PyQGIS_Repo/Graf_viari/BCN_GrafVial_Trams_ETRS89_SHP.shp",
+    #"Graf_nodes": "C:/projectes_git/Dades/PyQGIS_Repo/Graf_viari/BCN_GrafVial_Nodes_ETRS89_SHP.shp"
 }
 
-# Diccionari buit dels diccionaris de capes per temàtica
-layers = {
+# Diccionari buit de diccionaris de capes per temàtica
+# Temàtica: diccionari de capes
+dict_layers = {
     "Limits_administratius": {},
+    "Urbanisme": {},
     "Graf": {}
 }
 
 # Diccionari buit dels índex de les capes
-indexs = {}
+dict_indexs = {}
+
 
 # Importació de les capes al projecte i generació dels índex espacials sobre les geometries de cada capa
 ## Límits administratius
 for i, (nom, path) in enumerate(capes_lim_adm.items()):
-    # Per a cada nom i ruta del diccionari de capes de límits administratius:
+    
     # Creació de la capa vectorial amb la ruta i utilitzant el nom
     layer = QgsVectorLayer(path, nom, "ogr")
+    
     # Comprovació que la capa és vàlida
     if not layer.isValid():
         print(f"Error al carregar la capa de {nom}!")
     else:
-        # Si la capa és vàlida:
         # Addició de la capa al projecte, sense afegir-la al llenç
         project.addMapLayer(layer, False)
+        
         # Inserció de la capa al panell de capes a la posició definida
         root.insertLayer(i, layer)
+        
         # Addició de la capa al diccionari de diccionaris de capes, al grup de Límits administratius
         # El key és el nom de la capa, i el value és la capa vectorial pròpiament (QgsVectorLayer)
-        layers["Limits_administratius"][nom] = layer
+        dict_layers["Limits_administratius"][nom] = layer
+        
         # Generació de l'índex i addició al diccionari d'índexs
-        indexs[nom] = QgsSpatialIndex(layer.getFeatures())
-## Graf viari
-for i, (nom, path) in enumerate(capes_graf_viari.items()):
-    # Per a cada nom i ruta del diccionari de capes del graf viari:
+        dict_indexs[nom] = QgsSpatialIndex(layer.getFeatures())
+
+## Urbanisme
+for i, (nom, path) in enumerate(capes_urb.items()):
+
     # Creació de la capa vectorial amb la ruta i utilitzant el nom
     layer = QgsVectorLayer(path, nom, "ogr")
+    
     # Comprovació que la capa és vàlida
     if not layer.isValid():
         print(f"Error al carregar la capa de {nom}!")
     else:
-        # Si la capa és vàlida:
         # Addició de la capa al projecte, sense afegir-la al llenç
         project.addMapLayer(layer, False)
+       
         # Inserció de la capa al panell de capes a la posició definida
         root.insertLayer(i, layer)
+       
+        # Addició de la capa al diccionari de diccionaris de capes, al grup d'Urbanisme
+        # El key és el nom de la capa, i el value és la capa vectorial pròpiament (QgsVectorLayer)
+        dict_layers["Urbanisme"][nom] = layer
+        
+        # Generació de l'índex i addició al diccionari d'índexs
+        dict_indexs[nom] = QgsSpatialIndex(layer.getFeatures())
+
+## Graf viari
+for i, (nom, path) in enumerate(capes_graf_viari.items()):
+
+    # Creació de la capa vectorial amb la ruta i utilitzant el nom
+    layer = QgsVectorLayer(path, nom, "ogr")
+    
+    # Comprovació que la capa és vàlida
+    if not layer.isValid():
+        print(f"Error al carregar la capa de {nom}!")
+    else:
+        # Addició de la capa al projecte, sense afegir-la al llenç
+        project.addMapLayer(layer, False)
+       
+        # Inserció de la capa al panell de capes a la posició definida
+        root.insertLayer(i, layer)
+       
         # Addició de la capa al diccionari de diccionaris de capes, al grup de Graf viari
         # El key és el nom de la capa, i el value és la capa vectorial pròpiament (QgsVectorLayer)
-        layers["Graf"][nom] = layer
+        dict_layers["Graf"][nom] = layer
+        
         # Generació de l'índex i addició al diccionari d'índexs
-        indexs[nom] = QgsSpatialIndex(layer.getFeatures())
+        dict_indexs[nom] = QgsSpatialIndex(layer.getFeatures())
